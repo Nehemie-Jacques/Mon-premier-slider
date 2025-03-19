e95db1193bc2d3bc90993d1edb1b0bd6a577761b
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-03-19 13:44:04 +0100

    my first slider in js

---
+++ src/slide.js
@@ -0,0 +1,11 @@
+const slide =  ["../img/paysage1.jpg", "../img/paysage2.jpg", "../img/paysage3.webp", "../img/paysage4.webp", "../img/paysage5.webp"];
+let numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero > slide.length - 1) numero = 0;
+    if (numero < 0) numero = slide.length - 1;
+   document.getElementById("paysage").src ="../img/" + slide[numero];
+}
+
+setInterval("ChangeSlide(1)", 4000) ;
