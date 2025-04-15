2429403c61601fc098769ecb90b1db75054990ac
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-04-15 13:32:03 +0100

    simplification du chemin d'image

--- assets/script/slide.js
+++ assets/script/slide.js
@@ -1,11 +1,11 @@
-const slide =  ["../img/paysage1.jpg", "../img/paysage2.jpg", "../img/paysage3.webp", "../img/paysage4.webp", "../img/paysage5.webp"];
+const slide =  ["paysage1.jpg", "paysage2.jpg", "paysage3.webp", "paysage4.webp", "paysage5.webp"];
 let numero = 0;
 
 function ChangeSlide(sens) {
-    numero = numero + sens;
+    numero += sens;
     if (numero > slide.length - 1) numero = 0;
     if (numero < 0) numero = slide.length - 1;
    document.getElementById("paysage").src ="./assets/img/" + slide[numero];
 }
 
-setInterval("ChangeSlide(1)", 4000);
+setInterval( () => ChangeSlide(1), 4000);
